8b4550699aecb128745668c8fdb2302da7b2f9e4
SKoN123456 <msued5203@gmail.com>
2026-05-29 16:17:30 -0700

    Add files via upload

--- tournament.py
+++ tournament.py
@@ -5,7 +5,7 @@ def getTournamentList():
     conn = get_db_connection()
     cursor = conn.cursor()
 
-    cursor.execute("SELECT * FROM Tournament ORDER BY t_name Asc;")
+    cursor.execute("SELECT * FROM \"Tournament\" ORDER BY t_name Asc;")
     tournamentresults = cursor.fetchall()
 
     cursor.close()
@@ -17,7 +17,7 @@ def selectTournament(t_tourneyid):
     conn = get_db_connection()
     cursor = conn.cursor()
 
-    cursor.execute("SELECT * FROM Tournament WHERE t_tourneyID = %s;", (t_tourneyid,))
+    cursor.execute("SELECT * FROM \"Tournament\" WHERE t_tourneyID = %s;", (t_tourneyid,))
     selectedTournament = cursor.fetchone()
 
     cursor.close()
@@ -29,7 +29,7 @@ def newTournament(name, tformat):
     conn = get_db_connection()
     cursor = conn.cursor()
 
-    cursor.execute("INSERT INTO TOURNAMENT(t_name, t_format) VALUES (%s, %s);", (name, tformat))
+    cursor.execute("INSERT INTO \"TOURNAMENT\"(t_name, t_format) VALUES (%s, %s);", (name, tformat))
     conn.commit()
 
     cursor.close()
@@ -41,7 +41,7 @@ def updateTournament(t_tourneyid, name, tformat):
     conn = get_db_connection()
     cursor = conn.cursor()
 
-    cursor.execute("UPDATE Tournament SET t_name = %s, t_format = %s WHERE t_tourneyid = %s;", (name, tformat, t_tourneyid))
+    cursor.execute("UPDATE \"Tournament\" SET t_name = %s, t_format = %s WHERE t_tourneyid = %s;", (name, tformat, t_tourneyid))
 
     conn.commit()
     cursor.close()
@@ -53,7 +53,7 @@ def deleteTournament(t_tourneyid):
     conn = get_db_connection()
     cursor = conn.cursor()
 
-    cursor.execute("DELETE FROM Tournament WHERE t_tourneyid = %s;", (t_tourneyid,))
+    cursor.execute("DELETE FROM \"Tournament\" WHERE t_tourneyid = %s;", (t_tourneyid,))
     conn.commit()
 
     cursor.close()
